157c7d47074c596b84e8cf000e36150f0bbf2683
charles <weiche.tsai@hotmail.com>
2019-06-25 11:11:05 +0800

    add viewpoint addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,10 @@
 import { configure } from '@storybook/react';
 import { addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+import { withA11y } from '@storybook/addon-a11y';
 
 addDecorator(withInfo);
+addDecorator(withA11y);
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
 function loadStories() {
