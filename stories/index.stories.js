6ae47decb16b5085fda24624d06153fb88742678
charles <weiche.tsai@hotmail.com>
2019-06-25 15:56:36 +0800

    add background addon

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -10,7 +10,13 @@ import { Button, Welcome } from '@storybook/react/demo';
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
-  .addParameters({ viewport: { defaultViewport: 'iphone6' }})
+  .addParameters({
+    viewport: { defaultViewport: 'iphone6'},
+    backgrounds: [
+      { name: 'twitter', value: '#00aced', default: true },
+      { name: 'facebook', value: '#3b5998' },
+    ],
+  })
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>
